116b1b1c53fbd60045e3bc70ab32f8a751812584
yara20031964 <yara200359@gmail.com>
2023-10-14 02:27:04 +0300

    added main.c to execute the program

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	inf_type info[] = { INF_INIT };
+	int f = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (f)
+		: "r" (f));
+
+	if (argc == 2)
+	{
+		f = open(argv[1], O_RDONLY);
+		if (f == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_puts(argv[0]);
+				_puts(": 0: Can't open ");
+				_puts(argv[1]);
+				_putchar('\n');
+				_putchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = f;
+	}
+	pop_env_list(info);
+	renum_history(info);
+	hash(info, argv);
+	return (EXIT_SUCCESS);
+}
+
